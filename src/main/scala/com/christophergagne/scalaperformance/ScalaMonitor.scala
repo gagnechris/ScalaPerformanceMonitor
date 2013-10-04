d41073278ef2f05b40e8046e6283265624fb3622
Chris Gagne <gagnechris@me.com>
2013-10-04 14:52:35 -0400

    Changes to not use JMXConsole at this time, simply write info to Logger

--- src/main/scala/com/christophergagne/scalaperformance/ScalaMonitor.scala
+++ src/main/scala/com/christophergagne/scalaperformance/ScalaMonitor.scala
@@ -5,21 +5,38 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.weaver.tools.JoinPointMatch
 
 import com.typesafe.scalalogging.slf4j.Logging
-import compat.Platform.currentTime
+import java.text.SimpleDateFormat
+import java.util.TimeZone
 
 @Aspect
 abstract class ScalaMonitor extends Logging {
-	@Around(value = "methodPerformancePointcut()")
-	def logMethodPerformance(jp: ProceedingJoinPoint) = {
-		val startTime = currentTime
-		jp.proceed()
-		val endTime = currentTime
-  	
-    // val current = Thread.currentThread
-    // val stack = current.getStackTrace()
-    // for(stackElement <- stack){
-    //   logger.debug(stackElement.toString)
-    // }
-		JMXClient.recordExecution(jp.getTarget.getClass.getName.toString, startTime, endTime)
+  val curTimeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+  curTimeFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+  @Around(value = "methodPerformancePointcut()")
+  def logMethodPerformance(jp: ProceedingJoinPoint) = {
+    val startTime = System.currentTimeMillis()
+    val startTimeString = curTimeFormat.format(startTime)
+    jp.proceed()
+    val endTime = System.currentTimeMillis()
+    val endTimeString = curTimeFormat.format(endTime)
+    val duration = endTime - startTime
+    val methodString = jp.toLongString()
+    
+    logger.info(s"method=$methodString, start=$endTimeString, end=$startTimeString, duration=$duration")
+  }
+
+	@Around(value = "methodPerformancePointcutWithReturn()")
+	def logMethodPerformanceWithReturn(jp: ProceedingJoinPoint) = {
+		val startTime = System.currentTimeMillis()
+    val startTimeString = curTimeFormat.format(startTime)
+		val result = jp.proceed()
+		val endTime = System.currentTimeMillis()
+    val endTimeString = curTimeFormat.format(endTime)
+    val duration = endTime - startTime
+    val methodString = jp.toLongString()
+
+  	logger.info(s"method=$methodString, start=$endTimeString, end=$startTimeString, duration=$duration")
+    result
 	}
 }
